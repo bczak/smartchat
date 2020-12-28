75de526dd07dcc5b96bf86df920bcea1b2117704
Jakhongir <tashpjak@fel.cvut.cz>
2020-12-28 23:44:10 +0000

    Added spinner. New error handling

--- src/client.js
+++ src/client.js
@@ -1,24 +1,33 @@
 import io from 'socket.io-client'
 import {handshake} from './protocol.js'
 import chalk from 'chalk'
+import ora from 'ora'
 
 export default class Client {
 	host
 	port
 	socket
-	
-	constructor(props) {
-		this.host = props.host
-		this.port = props.port
+
+	constructor (props) {
+		this.host=props.host
+		this.port=props.port
 	}
-	
-	async connect(callback) {
-		const socket = io(`http://${this.host}:${this.port}`)
-		this.socket = socket
-		this.socket.on('connect', function () {
+
+	async connect (callback) {
+		const socket=io(`http://${this.host}:${this.port}`)
+		this.socket=socket
+		const spinner=ora("Trying to connect...").start()
+		this.socket.on('connect',function() {
 			socket.send(handshake())
 			console.log(chalk.green('Connected'))
+			spinner.stop()
 			return callback()
 		})
+		setTimeout(function() {
+			spinner.stop()
+			console.log(chalk.red('Connection failed'));
+			return callback()
+		},5000)
+
 	}
 }
