27e53c88415d4fa2b515e75509eabcd408ea5a48
Jakhongir <tashpjak@fel.cvut.cz>
2021-01-20 17:45:41 +0100

    Another bugfix

--- src/commands/log.js
+++ src/commands/log.js
@@ -2,7 +2,7 @@ import {getLogs} from '../memory.js'
 
 export async function apply(cli) {
 	cli
-		.command('election')
+		.command('logs')
 		.action(async () => {
 			console.log((await getLogs()).join('\n'))
 		})
